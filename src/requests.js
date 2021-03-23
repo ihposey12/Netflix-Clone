bbce9e06636de47e4b8e065cee2c66b3ad64795d
ihposey12 <ihposey@crimson.ua.edu>
2021-03-22 17:45:11 -0700

    fetch all endpoints/style ui banner

---
+++ src/requests.js
@@ -0,0 +1,14 @@
+const APIKEY = 'c0c4271b5138fae4e794a38126031f58'
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${APIKEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${APIKEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${APIKEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${APIKEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${APIKEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${APIKEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${APIKEY}&with_genres=10749`,
+    fetchDocumentariesMovies: `/discover/movie?api_key=${APIKEY}&with_genres=99`
+}
+
+export default requests
